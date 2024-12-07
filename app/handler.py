48603eab81d11a7ec31074ce1ba6384260fc6c44
LAPTOP-7EEHOF98\fedor <fedor@agafonov.org>
2024-12-07 06:17:41 +0300

    Регистрация в боте

--- app/handler.py
+++ app/handler.py
@@ -6,10 +6,68 @@ from aiogram.fsm.context import FSMContext
 
 from datetime import datetime as dt
 
+import app.keyboard as kb
+
 router = Router()
+
+
+class Register(StatesGroup):
+    id = State()
+    username_tg = State()
+    name = State()
+    phone = State()
 
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message, state: FSMContext, bot: Bot):
     await state.clear()
+    # Разное ссобщение в зависимости наличия пользователя с id
     await message.answer("Привет, Я мини-игра от МТС! 👋\nСкорее давай играть!")
+    await message.answer("Регистрируйся скорее!", reply_markup=kb.start_register)
+
+
+@router.callback_query(F.data == 'start_register')
+async def register_start_handler(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(Register.name)
+    await callback.message.edit_reply_markup(reply_markup=None)
+    await callback.message.answer("Введи свое имя")
+
+
+@router.message(Register.name)
+async def register_name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.update_data(id=message.from_user.id)
+    await state.update_data(username_th=message.from_user.username)
+    await state.set_state(Register.phone)
+    await message.answer("Введи свое имя")
+
+
+@router.message(Register.phone)
+async def register_phone(message: Message, state: FSMContext):
+    await state.update_data(phone=message.text)
+
+    data = await state.get_data()
+
+    await message.answer(f'Тебя зовут: {data['name']}\n'
+                         f'Телефон: {data['phone']}',
+                         reply_markup=kb.pass_register)
+
+
+@router.callback_query(F.data == 'pass_register')
+async def register_pass(callback: CallbackQuery, state: FSMContext, bot: Bot):
+    today_now = dt.today().date()
+
+    data = await state.get_data()
+
+    # Записываем в бд
+    # Развилка для реферальных и самостоятельный регов для разных сообщений
+    await callback.message.answer("Ты успешно зарегестрирован! ⚡️\n",
+                                      reply_markup=kb.main)
+    await state.clear()
+
+
+@router.callback_query(F.data == 'change_register')
+async def register_change(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(Register.name)
+    await callback.message.edit_reply_markup(reply_markup=None)
+    await callback.message.answer("Введи твое имя")
---
+++ app/keyboard.py
@@ -0,0 +1,16 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types.web_app_info import WebAppInfo
+
+
+main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Запустить игру')],
+                                     [KeyboardButton(text='Обмен монет')],
+                                     [KeyboardButton(text='Профиль')]],
+                           resize_keyboard=True,
+                           input_field_placeholder='Выбери раздел нижу...')
+
+start_register = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="🆔 Зарегестрироваться 🆔", callback_data='start_register')]])
+
+pass_register = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Все верно ✅", callback_data='pass_register')],
+    [InlineKeyboardButton(text="Изменить данные 🔄", callback_data='change_register')]])
